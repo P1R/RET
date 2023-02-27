103e5513fdb504bae75e1344606dd20f0ae16f9c
David E. Perez Negron R <david@neetsec.com>
2023-02-27 00:03:02 -0600

    update Rebuilding from scratch

--- contracts/RET.sol
+++ contracts/RET.sol
@@ -1,7 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+
 pragma solidity ^0.7.0;
 
 contract RET {
-    address public contractOwner;
+    address public owner;
     uint public currentAge;
     uint public retirementAge;
     uint public distributionPeriod;
@@ -13,7 +15,7 @@ contract RET {
     uint public iAmDead;
 
     constructor(uint _currentAge, uint _retirementAge, uint _distributionPeriod, address[] memory _relatives, uint[] memory _percentages, uint _iAmAlive) {
-        contractOwner = msg.sender;
+        owner = msg.sender;
         currentAge = _currentAge;
         retirementAge = _retirementAge;
         distributionPeriod = _distributionPeriod * 1 weeks;
@@ -25,16 +27,21 @@ contract RET {
         iAmDead = block.timestamp + iAmAlive;
     }
 
-    function deposit() public payable {
-        require(msg.sender == contractOwner, "Only contract owner can deposit");
+    function deposit() payable external onlyOwner {
+        require(msg.value > 0 ether);
         lifeProof();
     }
-    // @notice this function executed by the contractOwner proofs he is alive
+    // @notice this function executed by the owner proofs he is alive
     // and resets the iAmAlive
-    function lifeProof() public {
-        require(msg.sender == contractOwner, "Only owner gives alive proof");
+    function lifeProof() public onlyOwner {
         require(block.timestamp < iAmAlive, "Keep alive time has passed");
         iAmDead = block.timestamp + iAmAlive;
+    }
+
+    modifier onlyOwner()
+    {
+        require (msg.sender == owner, "Caller is not the owner");
+        _;
     }
 
    // function requestRetirement() public {
